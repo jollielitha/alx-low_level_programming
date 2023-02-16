eea10d8db69582a4c05e7e9d4331d13ad91d1b7e
Jolly <jollie.litha@gmail.com>
2023-02-16 20:21:42 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: The function prints a message to the console using print()
+ * Return: always 0 (Success)
+ */
+
+int main (void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
