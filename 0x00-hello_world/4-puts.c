009649c1e3728eafa9e746c3d70382e8f29b10df
Jolly <jollie.litha@gmail.com>
2023-02-16 20:05:06 +0000

    Puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-/** 
+/**
  * main - Entry Point
  *
  * Description:  puts() function  prints a message to the console
  *
- * Return: Always 0 (Success)	
+ * Return:Always 0 (Success)
  */
 
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
