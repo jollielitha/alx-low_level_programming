254e058ba3260282fe62162eaab52430925e677e
Jolly <jollie.litha@gmail.com>
2023-02-16 20:47:50 +0000

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  * Description:  The function printf() prints output to consule
- * return: always (0) success
+ * return: always 0 (success)
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof (char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a lonf int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
