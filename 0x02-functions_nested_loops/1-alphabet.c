a2189b3ea2fe8ba43129b2d9fcc3e3ffca4600ca
Jolly <jollie.litha@gmail.com>
2023-02-21 20:58:54 +0000

    Lowercase Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabets in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char alph
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}
