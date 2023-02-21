3b7ecc53951d16a82cbe8f8ab7d2f66cefe3e7a3
Jolly <jollie.litha@gmail.com>
2023-02-21 21:59:20 +0000

    Last print

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - calculated absolute integer value 
+ * @n: the int to check
+ * Return: the absolute integer value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
