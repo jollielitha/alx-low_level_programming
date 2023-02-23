e8c8e93d2ff075e2d837bf3aa79425fcf3cca055
jollielitha <jollie.litha@gmail.com>
2023-02-23 23:54:00 +0000

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,26 +9,22 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
- int largest;
 
- if (a > b && a > c)
- {
- largest = a;
- }
- else if (a > b && c > a)
- {
- largest = c;
- }
- else if (b > c)
- {
- largest = b;
- }
- else
- {
- largest = c;
- }
+	int largest;
 
-return (largest);
+	if (a >= b && a >= c)
+	{
+	largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+	return (largest);
 }
-
