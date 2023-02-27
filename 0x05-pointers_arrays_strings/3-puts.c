3e7d256222028b79383bffad85003e7a416ed8b0
jollielitha <jollie.litha@gmail.com>
2023-02-27 21:03:22 +0000

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar(\n);
+}
