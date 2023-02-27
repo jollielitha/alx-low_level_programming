05075189da3ea3b67a17d5694b07f21c7fd0417c
jollielitha <jollie.litha@gmail.com>
2023-02-27 21:44:27 +0000

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -21,3 +27,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+#endif
