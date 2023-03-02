cf4de74e0f9654191804873ddc549301bafd25a5
jollielitha <jollie.litha@gmail.com>
2023-03-02 13:36:54 +0000

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,17 @@
  */
 
 char *string_toupper(char *str)
+
 {
-    int i;
-    for (i=0; str[i]; i++)
-    {
-        if (str[i] >= 97 && str[i] <= 122)
-        {
-            str[i] -= 32;
-        }
-    }
+
+	int i = 0;
+
+	while (str[i])
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] -= 32;
+	i++;
+	}
+	return (str);
+
+}
