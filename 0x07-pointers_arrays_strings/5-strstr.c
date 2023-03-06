2c41002f97a525caa00f02e59b7c53e84cb9f092
jollielitha <jollie.litha@gmail.com>
2023-03-06 14:39:59 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strstr - unction finds the first occurrence of the substring 
- * needle in the string haystack. The terminating 
+ * _strstr - unction finds the first occurrence of the substring
+ * needle in the string haystack. The terminating
  * null bytes (\0) are not compared
  * @haystack: string to be searched
  * @needle: substring to be found
